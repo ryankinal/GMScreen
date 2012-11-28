ca549dcea093fd7d530e73d91b42061002a7b47b
Ryan Kinal <ryan.kinal@gmail.com>
2012-11-27 20:43:31 -0500

    Removed dom functions from item types

--- item-types/item-types.js
+++ item-types/item-types.js
@@ -1,42 +1,4 @@
-var itemTypes = {},
-    dom = {
-        create: function(tagName, context)
-        {
-            context = context || document;
-
-            return context.createElement(tagName)
-        },
-        text: function(value, context)
-        {
-            context = context || document;
-            value = value || '';
-
-            return context.createTextNode(value);
-        },
-        bind: function(element, type, listener)
-        {
-            return element.addEventListener(type, listener);
-        },
-        empty: function(element)
-        {
-            while (element.childNodes.length)
-            {
-                element.removeChild(element.firstChild);
-            }
-        },
-        inDocument: function(element)
-        {
-            do 
-            {
-                if (element === document)
-                {
-                    return true;
-                }
-            } while (element = element.parentNode);
-
-            return false;
-        }
-    };
+var itemTypes = {};
 
 itemTypes.base = {
     load: function(data) {
