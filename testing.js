1b9d8fe6246cd9d71ed0822594ca282729545561
Ryan Kinal <ryan.kinal@gmail.com>
2012-12-02 15:22:09 -0500

    Updated testing.js to use the controller

--- testing.js
+++ testing.js
@@ -1,59 +1,35 @@
-var windowSet = Object.create(UIWindowSet),
-    table = Object.create(itemTypes.table),
-    tableWindow = Object.create(UIWindow),
-    list = Object.create(itemTypes.unorderedList),
-    listWindow = Object.create(UIWindow),
-    list2 = Object.create(itemTypes.orderedList),
-    list2Window = Object.create(UIWindow);
-
-windowSet.init('Fuck You, That\'s Why');
-tableWindow.init('Initiative Order', dom.getById('screen'));
-listWindow.init('Shopping List', dom.getById('screen'));
-list2Window.init('Steps to success', dom.getById('screen'));
-
-windowSet.addWindow(tableWindow);
-windowSet.addWindow(listWindow);
-windowSet.addWindow(list2Window);
-windowSet.render();
-
-table.load({
-    headers:
-    [
-        'Character Name',
-        'Initiative',
-        'Perception'
-    ],
-    body:
-    [
+var controller = Object.create(ScreenController),
+    windowSet = controller.addWindowSet('Testing'),
+    tableWindow = controller.addTable('Initiative Order', {
+        headers:
         [
-            'JR',
-            27,
-            4
+            'Character Name',
+            'Initiative',
+            'Perception'
         ],
+        body:
         [
-            'Eric',
-            14,
-            30
+            [
+                'JR',
+                27,
+                4
+            ],
+            [
+                'Eric',
+                14,
+                30
+            ]
         ]
-    ]
-});
-
-table.render(tableWindow.content);
-
-list.load([
-    'Milk',
-    'Eggs',
-    'Rice',
-    'Apples',
-    'Tofu'
-]);
-
-list.render(listWindow.content);
-
-list2.load([
-    'Steal underpants',
-    '...',
-    'Profit'
-]);
-
-list2.render(list2Window.content);
+    }),
+    orderedlistWindow = controller.addUnorderedList('Shopping List', [
+        'Milk',
+        'Eggs',
+        'Rice',
+        'Apples',
+        'Tofu'
+    ]),
+    unorderedListWindow = controller.addOrderedList('Steps to Success', [
+        'Steal underpants',
+        '...',
+        'Profit'
+    ]);
