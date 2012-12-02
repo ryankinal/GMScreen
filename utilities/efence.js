3a85b9b1713153deca5b9cd097c9731fade70edb
Ryan Kinal <ryan.kinal@gmail.com>
2012-12-02 15:50:51 -0500

    Added simple pub/sub library

---
+++ utilities/efence.js
@@ -0,0 +1,46 @@
+efence = (function() {
+	var eventSubscriptions = {};
+
+	return {
+		pub: function(eventName, data)
+		{
+			var i;
+
+			if (typeof eventSubscriptions[eventName] !== 'undefined')
+			{
+				for (i = 0; i < eventSubscriptions[eventName].length; i++)
+				{
+					eventSubscriptions[eventName][i](data);
+				}
+			}
+		},
+		sub: function(eventName, callback)
+		{
+			if (typeof eventSubscriptions[eventName] === 'undefined')
+			{
+				eventSubscriptions[eventName] = [];
+			}
+
+			eventSubscriptions[eventName].push(callback);
+
+			return callback;
+		},
+		unsub: function(eventName, callback)
+		{
+			var i;
+
+			if (typeof eventSubscriptions[eventName] !== 'undefined')
+			{
+				for (i = 0; i < eventSubscriptions[eventName].length; i++)
+				{
+					if (eventSubscriptions[eventName][i] === callback)
+					{
+						return eventSubscriptions[eventName].splice(i, 1);
+					}
+				}
+			}
+
+			return false;
+		}
+	}
+})();
