f230636b1906ee832340ffee1b1692f28cfb157e
Ryan Kinal <ryan.kinal@gmail.com>
2012-12-05 16:59:29 -0500

    Added UI controls

---
+++ app/controls.js
@@ -0,0 +1,95 @@
+(function(pubsub) {
+    var windowSetList = dom.getById('windowSets'),
+        currentSetDisplay = dom.getById('currentSetName'),
+        optionsList = dom.getById('currentSetOptions'),
+        currentSetShaded = false,
+        renderWindowSets = function(parent)
+        {
+            var i,
+                fragment = dom.fragment(),
+                current = dom.create('span'),
+                all = dom.create('ul'),
+                item,
+                itemLink,
+                sets = ScreenController.getWindowSets(),
+                currentSet = ScreenController.getCurrentWindowSet(),
+                newWindow = dom.create('li');
+
+            current.appendChild(dom.text('Your Screens'));
+            fragment.appendChild(current);
+
+            for (i = 0; i < sets.length; i++)
+            {
+                item = dom.create('li');
+                item.className = (i === currentSet.index) ? 'window-set current' : 'window-set';
+                item.dataset.index = i;
+                item.appendChild(dom.text(sets[i].name));
+                all.appendChild(item);
+            }
+
+            newWindow.appendChild(dom.text('New Screen...'));
+            newWindow.className = 'new-window-set';
+
+            fragment.appendChild(all);
+            dom.empty(windowSetList);
+            windowSetList.appendChild(fragment);
+
+            dom.empty(currentSetDisplay);
+            currentSetDisplay.appendChild(dom.text(currentSet.set.name));
+        },
+        setListClickHandler = function(e)
+        {
+            e = e || window.event;
+
+            var target  = e.target || e.srcElement,
+                sets = ScreenController.getWindowSets(),
+                index = target.dataset.index,
+                set;
+
+            if (target.className === 'window-set')
+            {
+                ScreenController.getCurrentWindowSet().set.hide();
+                ScreenController.changeWindowSet(index).show();
+                renderWindowSets(windowSetList);
+            }
+            else if (target.className === 'new-window-set')
+            {
+                // show interface for adding new window set
+            }
+        },
+        optionsClickHandler = function(e)
+        {
+            e = e || window.event;
+
+            var target = e.target || e.srcElement,
+                text;
+
+            if (target.className === 'new-window')
+            {
+                // show interface for adding new window
+            }
+            else if (target.className === 'shade')
+            {
+                text = currentSetShaded ? 'Shade All' : 'Unshade All';
+                currentSetShaded = !currentSetShaded;
+                ScreenController.getCurrentWindowSet().set.shade(currentSetShaded);
+                dom.empty(target);
+                target.appendChild(dom.text(text));
+            }
+            else if (target.className === 'delete')
+            {
+                ScreenController.removeWindowSet(ScreenController.getCurrentWindowSet().index).hide();
+                ScreenController.getCurrentWindowSet().set.show();
+                renderWindowSets(windowSetList);
+            }
+        };
+
+
+    windowSetList.addEventListener('click', setListClickHandler);
+    optionsList.addEventListener('click', optionsClickHandler);
+    
+    
+    window.addEventListener('load', function(e) {
+        renderWindowSets(windowSetList);
+    });
+})(efence);
